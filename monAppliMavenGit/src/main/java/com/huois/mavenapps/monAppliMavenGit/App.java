a46f203738905e32d8969847dc27cb453c899432
HUIOS <services.huios@gmail.com>
2017-09-12 10:26:16 +0200

    commit projet initial

---
+++ monAppliMavenGit/src/main/java/com/huois/mavenapps/monAppliMavenGit/App.java
@@ -0,0 +1,13 @@
+package com.huois.mavenapps.monAppliMavenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
